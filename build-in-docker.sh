8ee138d4e5ba2bd3adf3a73d2515c0f6c60e94c4
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2023-01-26 16:47:42 +0200

    Build in Docker using alpine image to match Ghost
    Something to do with musl and glibc

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --env npm_config_cache='/ghost-cloudflare-r2/npm_cache' --volume "$(pwd)":/ghost-cloudflare-r2 --entrypoint "/bin/bash" node:18.13.0 /ghost-cloudflare-r2/build.sh
+docker run --rm --env npm_config_cache='/ghost-cloudflare-r2/npm_cache' --volume "$(pwd)":/ghost-cloudflare-r2 --entrypoint "/bin/sh" node:18.13.0-alpine /ghost-cloudflare-r2/build.sh
--- build.sh
+++ build.sh
@@ -1,19 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "Building ghost-cloudflare-r2..."
-# https://stackoverflow.com/a/246128
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# https://stackoverflow.com/a/1638397
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPT_DIR=$(dirname "$SCRIPT")
 
 set -x
 
 cd $SCRIPT_DIR
+
+# Cleanup
 rm -fr build/
+rm -fr node_modules/
+rm -fr ${SCRIPT_DIR}/npm_cache
 rm -f index.js index.d.ts index.js.map
 
+# Install
 npm_config_cache=${SCRIPT_DIR}/npm_cache npm install
+
+# Cleanup
 rm -fr ${SCRIPT_DIR}/npm_cache
 
+# Install
 cp ./build/src/index.js .
 cp ./build/src/index.js.map .
 cp ./build/src/index.d.ts .
