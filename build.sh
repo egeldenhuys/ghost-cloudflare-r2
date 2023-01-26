fd139ba4e8d2a8dfeaad06b69ad279df821365ff
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2023-01-26 13:07:07 +0200

    Copy js source map

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ set -x
 
 cd $SCRIPT_DIR
 rm -fr build/
-rm -f index.js index.d.ts
+rm -f index.js index.d.ts index.js.map
 npm_config_cache='/npm_cache' npm install
 
 cp ./build/src/index.js .
+cp ./build/src/index.js.map .
 cp ./build/src/index.d.ts .
-
