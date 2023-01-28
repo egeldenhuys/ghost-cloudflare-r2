1e385d29e0615955296220edd00293027887356a
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2023-01-28 21:10:56 +0200

    Merge env variables with Ghost config

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import {
 import path from 'path';
 import {readFile} from 'fs';
 
-interface FileInfo extends StorageBase.Image {
+export interface FileInfo extends StorageBase.Image {
   originalname: string;
   ext: string;
 }
@@ -31,12 +31,6 @@ function stripLeadingSlash(s: string): string {
 
 function stripEndingSlash(s: string): string {
   return s.indexOf('/') === s.length - 1 ? s.substring(0, s.length - 1) : s;
-}
-
-function check_env_variable(variableName: string) {
-  if (process.env[variableName] === undefined) {
-    throw new Error(`Environment variable ${variableName} is not defined`);
-  }
 }
 
 function readFileAsync(filePath: string): Promise<Buffer> {
@@ -77,10 +71,133 @@ interface Config {
   storage_type_images?: boolean;
   storage_type_media?: boolean;
   storage_type_files?: boolean;
+  GHOST_STORAGE_ADAPTER_R2_DOMAIN?: string;
+  GHOST_STORAGE_ADAPTER_R2_BUCKET?: string;
+  GHOST_STORAGE_ADAPTER_R2_ENDPOINT?: string;
+  GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID?: string;
+  GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY?: string;
+  GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX?: string;
+  GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX?: string;
+  GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX?: string;
+  GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES?: boolean;
+  GHOST_STORAGE_ADAPTER_R2_RESIZE_WIDTHS?: string;
+  GHOST_STORAGE_ADAPTER_R2_UUID_NAME?: boolean;
+  GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY?: number;
+  GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL?: boolean;
+}
+
+function mergeConfigWithEnv(config: Config): Config {
+  config.GHOST_STORAGE_ADAPTER_R2_DOMAIN =
+    process.env.GHOST_STORAGE_ADAPTER_R2_DOMAIN ||
+    config.GHOST_STORAGE_ADAPTER_R2_DOMAIN;
+
+  config.GHOST_STORAGE_ADAPTER_R2_BUCKET =
+    process.env.GHOST_STORAGE_ADAPTER_R2_BUCKET ||
+    config.GHOST_STORAGE_ADAPTER_R2_BUCKET;
+
+  config.GHOST_STORAGE_ADAPTER_R2_ENDPOINT =
+    process.env.GHOST_STORAGE_ADAPTER_R2_ENDPOINT ||
+    config.GHOST_STORAGE_ADAPTER_R2_ENDPOINT;
+
+  config.GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID =
+    process.env.GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID ||
+    config.GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID;
+
+  config.GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY =
+    process.env.GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY ||
+    config.GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY;
+
+  config.GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX =
+    process.env.GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX ||
+    config.GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX ||
+    '/content/images/';
+  config.GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX =
+    process.env.GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX ||
+    config.GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX ||
+    '/content/media/';
+  config.GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX =
+    process.env.GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX ||
+    config.GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX ||
+    '/content/files/';
+
+  let responsiveImages: boolean | undefined;
+  if (
+    process.env.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES &&
+    process.env.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES === 'true'
+  ) {
+    responsiveImages = true;
+  }
+  config.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES =
+    responsiveImages ||
+    config.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES ||
+    false;
+
+  config.GHOST_STORAGE_ADAPTER_R2_RESIZE_WIDTHS =
+    process.env.GHOST_STORAGE_ADAPTER_R2_RESIZE_WIDTHS ||
+    config.GHOST_STORAGE_ADAPTER_R2_RESIZE_WIDTHS ||
+    '300,600,1000,1600,400,750,960,1140,1200';
+
+  let uuidName: boolean | undefined;
+  if (
+    process.env.GHOST_STORAGE_ADAPTER_R2_UUID_NAME &&
+    process.env.GHOST_STORAGE_ADAPTER_R2_UUID_NAME === 'true'
+  ) {
+    uuidName = true;
+  }
+  config.GHOST_STORAGE_ADAPTER_R2_UUID_NAME =
+    uuidName || config.GHOST_STORAGE_ADAPTER_R2_UUID_NAME || false;
+
+  let jpegQuality: number | undefined;
+  if (process.env.GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY) {
+    jpegQuality = parseInt(
+      process.env.GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY
+    );
+  }
+  config.GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY =
+    jpegQuality || config.GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY || 80;
+
+  let saveOriginal: boolean | undefined;
+  if (
+    process.env.GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL &&
+    process.env.GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL === 'true'
+  ) {
+    saveOriginal = true;
+  }
+  config.GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL =
+    saveOriginal || config.GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL || false;
+  return config;
+}
+
+function checkConfig(config: Config) {
+  if (!config.GHOST_STORAGE_ADAPTER_R2_DOMAIN) {
+    throw new Error(
+      'Environment/config variable "GHOST_STORAGE_ADAPTER_R2_DOMAIN" has not been set'
+    );
+  }
+  if (!config.GHOST_STORAGE_ADAPTER_R2_BUCKET) {
+    throw new Error(
+      'Environment/config variable "GHOST_STORAGE_ADAPTER_R2_BUCKET" has not been set'
+    );
+  }
+  if (!config.GHOST_STORAGE_ADAPTER_R2_ENDPOINT) {
+    throw new Error(
+      'Environment/config variable "GHOST_STORAGE_ADAPTER_R2_ENDPOINT" has not been set'
+    );
+  }
+  if (!config.GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID) {
+    throw new Error(
+      'Environment/config variable "GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID" has not been set'
+    );
+  }
+  if (!config.GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY) {
+    throw new Error(
+      'Environment/config variable "GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY" has not been set'
+    );
+  }
 }
 
 export default class CloudflareR2Adapter extends StorageBase {
-  S3: S3Client;
+  private S3: S3Client;
   private bucket: string;
   private pathPrefix: string;
   private domain: string;
@@ -88,52 +205,40 @@ export default class CloudflareR2Adapter extends StorageBase {
   private imagesUrlPrefix: string;
   private mediaUrlPrefix: string;
   private filesUrlPrefix: string;
-  private responsiveImages: string;
+  private responsiveImages: boolean;
   private resizeWidths: number[];
   public saveRaw: unknown = undefined;
-  private uuidName: string;
-  private saveOriginal: string;
+  private uuidName: boolean;
+  private saveOriginal: boolean;
   private jpegQuality: number | undefined;
+  private config: Config;
 
   constructor(config: Config = {}) {
     log.debug('Initialising ghost-cloudflare-r2 storage adapter');
     super();
+    this.config = mergeConfigWithEnv(config);
+    checkConfig(config);
 
-    check_env_variable('GHOST_STORAGE_ADAPTER_R2_DOMAIN');
-    check_env_variable('GHOST_STORAGE_ADAPTER_R2_BUCKET');
-    check_env_variable('GHOST_STORAGE_ADAPTER_R2_ACCOUNT_ID');
-    check_env_variable('GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID');
-    check_env_variable('GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY');
+    this.bucket = <string>config.GHOST_STORAGE_ADAPTER_R2_BUCKET;
+    this.domain = <string>config.GHOST_STORAGE_ADAPTER_R2_DOMAIN;
+    this.imagesUrlPrefix = <string>(
+      config.GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX
+    );
+    this.mediaUrlPrefix = <string>(
+      config.GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX
+    );
+    this.filesUrlPrefix = <string>(
+      config.GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX
+    );
+    this.responsiveImages = <boolean>(
+      config.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES
+    );
 
-    this.bucket = process.env.GHOST_STORAGE_ADAPTER_R2_BUCKET || '';
-    this.pathPrefix = process.env.GHOST_STORAGE_ADAPTER_R2_PATH_PREFIX || '';
-    this.domain = process.env.GHOST_STORAGE_ADAPTER_R2_DOMAIN || '';
-    this.imagesUrlPrefix =
-      process.env.GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX ||
-      '/content/images/';
-    this.mediaUrlPrefix =
-      process.env.GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX ||
-      '/content/media/';
-    this.filesUrlPrefix =
-      process.env.GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX ||
-      '/content/files/';
-    this.responsiveImages =
-      process.env.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES || 'false';
-    this.resizeWidths = (
-      process.env.GHOST_STORAGE_ADAPTER_R2_RESIZE_WIDTHS || '600,1000,1600,2400'
-    )
+    this.resizeWidths = (<string>config.GHOST_STORAGE_ADAPTER_R2_RESIZE_WIDTHS)
       .split(',')
       .map(w => parseInt(w));
 
-    this.uuidName = process.env.GHOST_STORAGE_ADAPTER_R2_UUID_NAME || 'false';
-    this.saveOriginal =
-      process.env.GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL || 'true';
-
-    this.jpegQuality = process.env.GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY
-      ? parseInt(process.env.GHOST_STORAGE_ADAPTER_R2_RESIZE_JPEG_QUALITY)
-      : undefined;
-
-    if (this.responsiveImages === 'true') {
+    if (config.GHOST_STORAGE_ADAPTER_R2_RESPONSIVE_IMAGES === true) {
       // Ghost checks if a 'saveRaw' function exists on the storage adapter,
       // if it exists, the theme will generate srcset attribute in the HTML.
       this.saveRaw = function () {};
@@ -141,17 +246,26 @@ export default class CloudflareR2Adapter extends StorageBase {
 
     if (config.storage_type_images === true) {
       this.storageType = StorageType.Images;
-      this.pathPrefix = this.imagesUrlPrefix;
+      this.pathPrefix = <string>(
+        config.GHOST_STORAGE_ADAPTER_R2_IMAGES_URL_PREFIX
+      );
     } else if (config.storage_type_media === true) {
       this.storageType = StorageType.Media;
-      this.pathPrefix = this.mediaUrlPrefix;
+      this.pathPrefix = <string>(
+        config.GHOST_STORAGE_ADAPTER_R2_MEDIA_URL_PREFIX
+      );
     } else if (config.storage_type_files === true) {
       this.storageType = StorageType.Files;
-      this.pathPrefix = this.filesUrlPrefix;
+      this.pathPrefix = <string>(
+        config.GHOST_STORAGE_ADAPTER_R2_FILES_URL_PREFIX
+      );
     } else {
       this.storageType = StorageType.Images;
       this.pathPrefix = this.imagesUrlPrefix;
     }
+
+    this.uuidName = <boolean>config.GHOST_STORAGE_ADAPTER_R2_UUID_NAME;
+    this.saveOriginal = <boolean>config.GHOST_STORAGE_ADAPTER_R2_SAVE_ORIGINAL;
 
     log.info(
       'Cloudflare R2 Storage Adapter: handling',
@@ -162,14 +276,14 @@ export default class CloudflareR2Adapter extends StorageBase {
 
     this.S3 = new S3Client({
       region: 'auto',
-      endpoint: `https://${process.env.GHOST_STORAGE_ADAPTER_R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+      endpoint: <string>config.GHOST_STORAGE_ADAPTER_R2_ENDPOINT,
       credentials: {
-        accessKeyId: process.env.GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID || '',
-        secretAccessKey:
-          process.env.GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY || '',
+        accessKeyId: <string>config.GHOST_STORAGE_ADAPTER_R2_ACCESS_KEY_ID,
+        secretAccessKey: <string>(
+          config.GHOST_STORAGE_ADAPTER_R2_SECRET_ACCESS_KEY
+        ),
       },
     });
-
     log.debug('Initialisation done');
   }
 
@@ -372,7 +486,7 @@ export default class CloudflareR2Adapter extends StorageBase {
     const directory = this.getTargetDir(this.pathPrefix);
 
     return new Promise((resolve, reject) => {
-      if (this.saveOriginal === 'false' && this.isOriginalImage(fileInfo)) {
+      if (!this.saveOriginal && this.isOriginalImage(fileInfo)) {
         log.info(
           'Cloudflare R2 Storage Adapter: save(): discarding original: ',
           fileInfo.name
@@ -417,7 +531,7 @@ export default class CloudflareR2Adapter extends StorageBase {
               log.info('Saved', filePathR2);
               if (
                 this.isOriginalImage(fileInfo) &&
-                this.responsiveImages === 'true' &&
+                this.responsiveImages &&
                 this.storageType === StorageType.Images
               ) {
                 log.info('Generating different image sizes...');
