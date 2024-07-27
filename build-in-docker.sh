270855fa8cc5f827ad11c29bb2b8b01318507a52
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-07-27 07:29:39 +0200

    WIP: Github workflows

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-docker run --rm --env npm_config_cache='/ghost-cloudflare-r2/npm_cache' --volume "$(pwd)":/ghost-cloudflare-r2 --entrypoint "/bin/sh" node:18.13.0-alpine /ghost-cloudflare-r2/build.sh
+container_tool=podman
+
+if ! command -v $container_tool --version &> /dev/null
+then
+    echo "${container_tool} could not be found"
+    container_tool=docker
+fi
+
+if ! command -v $container_tool --version &> /dev/null
+then
+    echo "${container_tool} could not be found"
+    exit 1
+fi
+
+echo "using $container_tool"
+
+$container_tool run --rm \
+--env npm_config_cache='/ghost-cloudflare-r2/npm_cache' \
+--volume "$(pwd)":/ghost-cloudflare-r2:z \
+--entrypoint "/bin/sh" \
+node:18.13-alpine /ghost-cloudflare-r2/build.sh
