5a0bd04da1a966012382d597f655ac478dab4630
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-16 15:25:40 +0000

    Install playwright

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --env npm_config_cache='/ghost-cloudflare-r2/npm_cache' --volume "$(pwd)":/ghost-cloudflare-r2 --entrypoint "/bin/sh" node:18.13.0-alpine /ghost-cloudflare-r2/build.sh
+podman run --rm --env npm_config_cache='/ghost-cloudflare-r2/npm_cache' --volume "$(pwd)":/ghost-cloudflare-r2 --entrypoint "/bin/sh" node:20-alpine /ghost-cloudflare-r2/build.sh
