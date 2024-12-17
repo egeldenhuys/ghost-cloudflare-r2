bab04b1c8d5611aabbdcd83ea069dee3cf83fcad
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-17 16:26:39 +0200

    Working Hurl tests

--- integration-tests/hurl/images/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Create a random 32x32 png
-mx=32;my=32;head -c "$((3*mx*my))" /dev/urandom | magick -depth 8 -size "${mx}x${my}" RGB:- PNG:random.png
--- integration-tests/test.sh
+++ integration-tests/test.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "** Trapped CTRL-C"
+    exit 1
+}
+
 function run_hurl() {
     local hurl_args=$1
-    podman run --rm -v $PWD/hurl:/hurl:z,ro --userns=keep-id --workdir /hurl --net=host ghcr.io/orange-opensource/hurl:latest --file-root /hurl --variables-file /hurl/variables.env --jobs 1 --test ${hurl_args}
+    # --userns=keep-id
+    podman run --rm -v $PWD/hurl:/hurl:z,ro --workdir /hurl --net=host ghcr.io/orange-opensource/hurl:latest --file-root /hurl --variables-file /hurl/variables.env --jobs 1 --test ${hurl_args}
 }
+
+#podman-compose down
+#podman-compose up -d
 
 run_hurl fragments/0-init-blog.hurl
 run_hurl tests/upload-image.hurl
 run_hurl tests/upload-thumbnail.hurl
+run_hurl tests/upload-video.hurl
+run_hurl tests/upload-file.hurl
+
+#podman-compose down
