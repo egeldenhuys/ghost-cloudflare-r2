f4938270d98b1b8fdce7b691f1d29b736f903c64
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-19 21:06:25 +0200

    Concourse error when creating cache volume

--- ci/scripts/build-podman-compose-image.sh
+++ ci/scripts/build-podman-compose-image.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+echo "START"
+
 BUILD_DIR="$(pwd)"
 PODMAN_ROOT="${BUILD_DIR}/cache/storage"
+PODMAN_STORAGE_DRIVER=vfs
 
 ls -al
+ls -al $BUILD_DIR/cache
 ls -al $PODMAN_ROOT
 
-podman --root=$PODMAN_ROOT load -i podman-base-image/image.tar
-podman --root=$PODMAN_ROOT images
+podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER load -i podman-base-image/image.tar
+podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER images
 
-podman --root=$PODMAN_ROOT build -f ghost-cloudflare-r2/ci/images/podman-compose/Dockerfile -t localhost/podman-compose ghost-cloudflare-r2/ci/images/podman-compose
+podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER build -f ghost-cloudflare-r2/ci/images/podman-compose/Dockerfile -t localhost/podman-compose ghost-cloudflare-r2/ci/images/podman-compose
 
-podman --root=$PODMAN_ROOT images
+podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER images
 
-podman --root=$PODMAN_ROOT save localhost/podman-compose > image/image.tar
+podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER save localhost/podman-compose > image/image.tar
+
+ls -al
+ls -al $BUILD_DIR/cache
+ls -al $PODMAN_ROOT
+
+echo "DONE"
