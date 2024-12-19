eaa37bce1cdac89728d26051b994b61b4a312c35
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-19 21:23:55 +0200

    WIP: try podman registry cache

--- ci/scripts/build-podman-compose-image.sh
+++ ci/scripts/build-podman-compose-image.sh
@@ -3,8 +3,18 @@
 echo "START"
 
 BUILD_DIR="$(pwd)"
-PODMAN_ROOT="${BUILD_DIR}/cache/storage"
-PODMAN_STORAGE_DRIVER=vfs
+PODMAN_ROOT="/var/lib/containers/storage"
+PODMAN_STORAGE_DRIVER=overlay
+
+# Create a file, /etc/containers/registries.conf.d/local.conf
+cat > /etc/containers/registries.conf.d/local.conf <<EOF
+[[registry]]
+location = "192.168.88.20:5000"
+insecure = true
+
+EOF
+
+CACHE_COMMANDS="--layers --cache-from 192.168.88.20:5000/concourse/cache --cache-to 192.168.88.20:5000/concourse/cache"
 
 ls -al
 ls -al $BUILD_DIR/cache
@@ -13,7 +23,7 @@ ls -al $PODMAN_ROOT
 podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER load -i podman-base-image/image.tar
 podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER images
 
-podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER build -f ghost-cloudflare-r2/ci/images/podman-compose/Dockerfile -t localhost/podman-compose ghost-cloudflare-r2/ci/images/podman-compose
+podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER $CACHE_COMMANDS build -f ghost-cloudflare-r2/ci/images/podman-compose/Dockerfile -t localhost/podman-compose ghost-cloudflare-r2/ci/images/podman-compose
 
 podman --root=$PODMAN_ROOT --storage-driver=$PODMAN_STORAGE_DRIVER images
 
