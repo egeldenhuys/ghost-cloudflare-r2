85ea7616e572420fba8b83b9a831690e818feffd
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-19 16:25:44 +0200

    WIP: build using podman but lacks features

--- ci/scripts/build-podman-compose-image.sh
+++ ci/scripts/build-podman-compose-image.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 podman load -i podman-base-image/image.tar
+podman image ls
 
+podman build -f ghost-cloudflare-r2/ci/images/podman-compose/Dockerfile -t localhost/podman-compose ghost-cloudflare-r2/ci/images/podman-compose
+ls -al
+podman save localhost/podman-compose > podman-compose-image/image.tar
