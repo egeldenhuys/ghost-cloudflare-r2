ee7dcdba08138ec2b20e5e6b2e3f7efc809084fb
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-20 21:51:03 +0200

    WIP: download hurl and integration tests

--- integration-tests/hurl/media/generate.sh
+++ integration-tests/hurl/media/generate.sh
@@ -3,7 +3,8 @@
 OUTPUT_DIR=$1
 
 # Create a random 32x32 png
-mx=32;my=32;head -c "$((3*mx*my))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- PNG:${OUTPUT_DIR}/random.png
+# 'magick' could also be 'convert' on Ubuntu
+mx=32;my=32;head -c "$((3*mx*my))" /dev/urandom | magick -depth 8 -size "${mx}x${my}" RGB:- PNG:${OUTPUT_DIR}/random.png
 
 # Generate test video
 ffmpeg -f lavfi -i testsrc -t 10 -pix_fmt yuv420p ${OUTPUT_DIR}/test.mp4
--- integration-tests/test.sh
+++ integration-tests/test.sh
@@ -13,16 +13,19 @@ function ctrl_c() {
 function run_hurl() {
     local hurl_args=$1
     # --userns=keep-id
-    podman run --rm -v $PWD/hurl:/hurl:z,ro --workdir /hurl --net=host 192.168.88.20:5000/orange-opensource/hurl:5.0.1 --file-root /hurl --variables-file /hurl/variables.env --jobs 1 --test ${hurl_args}
+    #podman run --rm -v $PWD/hurl:/hurl:z,ro --workdir /hurl --net=host 192.168.88.20:5000/orange-opensource/hurl:5.0.1 --file-root /hurl --variables-file /hurl/variables.env --jobs 1 --test ${hurl_args}
+    ~/.local/bin/hurl --file-root ./hurl --variables-file ./hurl/variables.env --jobs 1 --test ${hurl_args}
 }
 
 # podman-compose down
 # podman-compose up -d
 
-run_hurl fragments/0-init-blog.hurl
-run_hurl tests/upload-image.hurl
-run_hurl tests/upload-thumbnail.hurl
-run_hurl tests/upload-video.hurl
-run_hurl tests/upload-file.hurl
+./hurl/media/generate.sh
+
+run_hurl ./hurl/fragments/0-init-blog.hurl
+run_hurl ./hurl/tests/upload-image.hurl
+run_hurl ./hurl/tests/upload-thumbnail.hurl
+run_hurl ./hurl/tests/upload-video.hurl
+run_hurl ./hurl/tests/upload-file.hurl
 
 # podman-compose down
