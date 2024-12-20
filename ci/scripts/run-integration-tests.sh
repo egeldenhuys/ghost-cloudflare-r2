c7f9337f2b47bb482d260c9f620ffa1407802d7e
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2024-12-20 13:22:48 +0200

    WIP: use registry mirrors

--- ci/scripts/run-integration-tests.sh
+++ ci/scripts/run-integration-tests.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 
 cat > /etc/containers/registries.conf.d/local.conf <<EOF
+# Configure insecure
 [[registry]]
 location = "192.168.88.20:5000"
 insecure = true
 
+[[registry]]
+location = "192.168.88.20:5001"
+insecure = true
+
+[[registry]]
+location = "192.168.88.20:5002"
+insecure = true
+
+[[registry]]
+location = "192.168.88.20:5003"
+insecure = true
+
+# Use mirrors
+[[registry]]
+prefix = "docker.io"
+location = "192.168.88.20:5001"
+insecure = true
+
+[[registry.mirror]]
+location = "192.168.88.20:5001"
+insecure = true
+
+[[registry]]
+prefix = "ghcr.io"
+location = "192.168.88.20:5002"
+insecure = true
+
+[[registry]]
+prefix = "quay.io"
+location = "192.168.88.20:5003"
+insecure = true
+
 EOF
 
-# podman images
-
-# podman load -i minio-image/image.tar
-# # podman tag "$(cat minio-image/digest)" "$(cat minio-image/repository):$(cat minio-image/tag)"
-
-# podman load -i ghost-image/image.tar
-# # podman tag "$(cat ghost-image/digest)" "$(cat ghost-image/repository):$(cat ghost-image/tag)"
-
-# podman load -i hurl-image/image.tar
-# # podman tag "$(cat hurl-image/digest)" "$(cat hurl-image/repository):$(cat hurl-image/tag)"
-
-# podman images
-
 cd ghost-cloudflare-r2/integration-tests
-# TODO: add to image
 
 podman-compose pull
 podman-compose up -d
